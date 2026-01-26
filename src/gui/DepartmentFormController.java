0a1deca580824976d026af4e6578c9f319c6b093
Rodrigo Araujo <rasch.dev@gmail.com>
2026-01-26 09:15:29 -0300

    Created DepartmentFormController::updateFormData. Updated DepartmentFormController, DepartmentListController, DepartmentListController::onNewDepartment and DepartmentListController::createDialogForm.

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,13 +9,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable {
+	private Department department;
 	@FXML private TextField txtID;
 	@FXML private TextField txtNAME;
 	@FXML private Label errorLabel;
 	@FXML private Button save;
 	@FXML private Button cancel;
+	
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
 	
 	@FXML public void onSave() {
 		System.out.println("onSave");
@@ -35,5 +41,13 @@ public class DepartmentFormController implements Initializable {
 		Constraints.setTextFieldInteger(txtID);
 		Constraints.setTextMaxLength(txtNAME, 30);
 	}
-
+	
+	public void updateFormData() {
+		if(department == null) {
+			throw new IllegalStateException("Null object.");
+		}
+		
+		txtID.setText(String.valueOf(department.getId()));
+		txtNAME.setText(department.getName());
+	}
 }
